32f994ed3afc13de4258782fb9cea3640a49a84d
weerza123zatereed <64904862+weerza123zatereed@users.noreply.github.com>
2020-05-27 23:13:06 +0700

    Create Mathfunction.py

---
+++ Mathfunction.py
@@ -0,0 +1,16 @@
+def AddNum(x,y):
+    print("ผลบวก","=",x+y)
+def SubtractNum(x,y):
+    print("ผลลบ","=",x-y)
+def MultiplyNum(x,y):
+    print("ผลคูณ","=",x*y)
+def DevideNum(x,y):
+    print("ผลหาร","=",x/y)
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+AddNum(x,y)
+SubtractNum(x,y)
+MultiplyNum(x,y)
+DevideNum(x,y)
